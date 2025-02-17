d91b66c4a4d4293bf6353c5eeee0dffe64b194fc
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-02-17 17:45:51 -0300

    express and nodemon working

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import * as fs from "fs";
+import express from "express";
 
-fs;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, Express with TypeScript!" });
+});
+
+app.listen(3000, () => {
+  console.log(`Server running`);
+});
